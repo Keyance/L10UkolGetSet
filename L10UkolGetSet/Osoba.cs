9bef5615075796e5f12d24596b62812e3fbac806
Keyance <klara.konopacova@seznam.cz>
2023-12-13 15:45:52 +0100

    Přidejte soubory projektu.

---
+++ L10UkolGetSet/Osoba.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10UkolGetSet
+{
+    internal class Osoba
+    {
+        private string Jmeno { get; set; }
+        private string prijmeni;
+        private string Prijmeni
+        {
+            get { return prijmeni; }
+            set { prijmeni = value; }
+        }
+
+        bool JePlnoleta { get; } = false;
+
+        private int vek;
+        int Vek
+        {
+            get
+            { return vek; }
+            set
+            {
+                if (value  < 0)
+                {
+                    Console.WriteLine("Nebyl zadán reálný věk osoby.");
+                }
+                else    
+                {
+                    vek = value;
+                    if (value > 18)
+                    {
+                        JePlnoleta = true; //toto zatím nefunguje
+                    }
+                }
+            }
+        }
+    }
+}
